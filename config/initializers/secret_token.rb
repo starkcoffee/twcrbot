16b734662a77848b8bd5ddea2165900fe53ff0a9
Mark Ryall <mark@ryall.name>
2011-03-06 21:04:53 +1000

    made rails secret configurable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Twrecruitbot::Application.config.secret_token = '1fbbfc8487d1ca0d0b15aeeae0f5d6479552b2e427f5b9e25dd9530190dd58c6d860548c3a32162c21ac664e00644b5571a41956374612866ea15fb4777a1f3d'
+Twrecruitbot::Application.config.secret_token = ENV['RAILS_SECRET'] || '1fbbfc8487d1ca0d0b15aeeae0f5d6479552b2e427f5b9e25dd9530190dd58c6d860548c3a32162c21ac664e00644b5571a41956374612866ea15fb4777a1f3d'
